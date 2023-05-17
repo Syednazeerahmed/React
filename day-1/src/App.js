3dfff96f42e608f534db79aac2ba77e1501dc4c2
SyedNaazeerAhmed <naz224598@gmail.com>
2023-05-17 08:18:20 +0530

    modified App and index

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
+// day-1 study
+
+// function App() {
+//   return <div className='App'>Hello world</div>;
+// }
+
+
+// day-2 studying
+const Name = "nazeer";
+const age = 21;
+const email = "nazeer@gmail.com";
+
 function App() {
+  
+  const Name1 = <h1>Hello - {Name}</h1>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {Name1}
+      
+      <br></br>
+
+      <div>
+        <h1>{Name}</h1>
+        <h2>{age}</h2>
+        <h2>{email}</h2>
+      </div>
+
+      {/*below is the example of component being used*/} 
+      <AboutNazeer />
+
+      {/*below is the example of component with props being used*/}
+      {/* <Users Name={Name} age={age} email={email} /> */}
+      <Users Name="mifzal"  age="19" email="mifzal@gmail.com" />
+      <Users Name='majeed' age='22' email='majeed@gmail.com' />
     </div>
   );
 }
 
+// Rules for creating component
+// 1. Always start component name with capital letter
+// 2. Component name should be unique
+
+//below is the function component
+ const AboutNazeer = () => {
+  return (
+    <div>
+      <h1>{Name}</h1>
+      <h2>{age}</h2>
+      <h2>{email}</h2>
+    </div>
+  );
+}
+
+//below is the funtion component with props
+const Users = (props) => {
+  return (
+    <div>
+      <h1>{props.Name}</h1>
+      <h2>{props.age}</h2>
+      <h2>{props.email} </h2>
+    </div>
+  );
+}
 export default App;
